ce336967807a893bd1b5a006c46b07c4b1069a7c
workfox <workfox@users.noreply.github.com>
2019-01-09 10:28:36 +1300

    azure sql test

--- Lk-starter/Program.cs
+++ Lk-starter/Program.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Lk_starter
@@ -14,12 +16,25 @@ namespace Lk_starter
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args);
+            MigrateDatabase(host);
+            host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static void MigrateDatabase(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider
+                                .GetRequiredService<Data.ApplicationDbContext>();
+                context.Database.Migrate();
+            }
+        }
+
+        public static IWebHost CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseApplicationInsights()
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .Build();
     }
 }
