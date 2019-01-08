2e89a2cff80055d9445a262f6b58bf3cb33cf83c
workfox <workfox@users.noreply.github.com>
2019-01-08 14:31:02 +1300

    show environment greeting

--- Lk-starter/Controllers/HomeController.cs
+++ Lk-starter/Controllers/HomeController.cs
@@ -5,14 +5,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Lk_starter.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace Lk_starter.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = configuration["Greeting"];
+            return View("Index", model);
         }
 
         public IActionResult About()
